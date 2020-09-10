f6260af0f6d32f12b6d2b95250e489205f2cc057
q775001 <q775001@USRTPWL67826.quintiles.net>
2020-09-10 14:16:59 -0400

    test

--- src/main/java/org/vaadin/spring/tutorial/TutorialApplication.java
+++ src/main/java/org/vaadin/spring/tutorial/TutorialApplication.java
@@ -2,11 +2,19 @@ package org.vaadin.spring.tutorial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.vaadin.server.VaadinServlet;
 
 @SpringBootApplication
 public class TutorialApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TutorialApplication.class, args);
+    }
+    
+    @Bean
+    public VaadinServlet vaadinServlet() {
+        return new OAuthServlet();
     }
 }
