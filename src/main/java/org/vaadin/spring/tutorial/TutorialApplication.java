96c71119338e9ec89b89c5ff3da8f99ce11f0b06
q775001 <q775001@USRTPWL67826.quintiles.net>
2020-09-11 08:32:08 -0400

    u

--- src/main/java/org/vaadin/spring/tutorial/TutorialApplication.java
+++ src/main/java/org/vaadin/spring/tutorial/TutorialApplication.java
@@ -20,53 +20,26 @@ public class TutorialApplication {
 		SpringApplication.run(TutorialApplication.class, args);
 	}
 
-	@Bean
+	/*@Bean
 	public VaadinServlet vaadinServlet() {
 		LOGGER.info("returning vaadin servlet...");
 		return new OAuthServlet();
-	}
+	}*/
 
-	/*
-	 * @Bean public WebServerFactoryCustomizer<TomcatServletWebServerFactory>
-	 * cookieProcessorCustomizer() {
-	 * 
-	 * return (factory) -> factory.addContextCustomizers((context) -> {
-	 * LegacyCookieProcessor legacyCookieProcessor = new LegacyCookieProcessor();
-	 * legacyCookieProcessor.setSameSiteCookies(SameSiteCookies.NONE.getValue());
-	 * context.setCookieProcessor(legacyCookieProcessor); });
-	 * 
-	 * }
-	 */
 	
-	@Bean
-	public EmbeddedServletContainerCustomizer customizer() {
-		return new EmbeddedServletContainerCustomizer() {
-
-			@Override
-			public void customize(ConfigurableEmbeddedServletContainer container) {
-				LOGGER.info("container: {}",container);
-				LOGGER.info("container: {}",container.getClass());
+	/*	@Bean
+		public EmbeddedServletContainerCustomizer customizer() {
+			return new EmbeddedServletContainerCustomizer() {
+	
+				@Override
+				public void customize(ConfigurableEmbeddedServletContainer container) {
+					LOGGER.info("container: {}",container);
+					LOGGER.info("container: {}",container.getClass());
+					
+				}
 				
-			}
-			
-		};
-		/*
-		 * return container -> { if (container instanceof
-		 * TomcatEmbeddedServletContainerFactory) {
-		 * TomcatEmbeddedServletContainerFactory tomcat =
-		 * (TomcatEmbeddedServletContainerFactory) container;
-		 * tomcat.addContextCustomizers(context -> context.setCookieProcessor(new
-		 * LegacyCookieProcessor())); } };
-		 */
-	}
+			};
 	
-	/*@EventListener
-	  public void onApplicationEvent(ContextRefreshedEvent event) {
-		ApplicationContext applicationContext = event.getApplicationContext();
-		
-		//CookieSerializer cs;
-	    DefaultCookieSerializer cookieSerializer = applicationContext.getBean(DefaultCookieSerializer.class);
-	    //log.info("Received DefaultCookieSerializer, Overriding SameSite Strict");
-	    cookieSerializer.setSameSite("None");
-	  }*/
+		}*/
+	
 }
